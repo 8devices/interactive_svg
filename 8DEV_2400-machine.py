aef7da04f2dbe47f0ac6df096cdd90a2e914d71b
vytasm <vytas@8devices.com>
2015-12-09 22:35:13 +0200

    Updated last merge that conflicted with Saulius commit
    Moved documentation to docs folder.
    Fixed Saulius error in Python file 8DEV_2400-mashine.py "TypeError: list
    indices must be integers, not str". Initialized variable as List but was
    used as dictionary...

--- 8DEV_2400-machine.py
+++ 8DEV_2400-machine.py
@@ -7,7 +7,7 @@ import time
 import sys
 
 
-PIN = []
+PIN = {}
 
 PIN['D0'] =  {"bubble1":"path5743", "text1":"text11311_13_", "bubble2":"path5750", "text2":"text11311_12_"}
 PIN['D1'] =  {"bubble1":"path5757", "text1":"text11311_15_", "bubble2":"path5764", "text2":"text11311_14_"}
@@ -51,22 +51,53 @@ def pp(a, b):
 
         # dummy button events - change color
         try:
-            if parsed_json['mousedown'] == 'START':
-                sys.stdout.write('{"arg1": "path11108", "arg2": "fill", "arg3": "set_property", "arg4": "#ff0000"}\r\n')
+            if parsed_json['mouseout'] == 'polygon10753':
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "polygon10753", "style": {"fill": "#1E1E1E"}}\r\n')
+                sys.stdout.flush()
+        except:
+            pass
+
+        try:
+            if parsed_json['mouseover'] == 'polygon10753':
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "polygon10753", "style": {"fill": "#8888FF"}}\r\n')
                 sys.stdout.flush()
         except:
             pass
 
         try:
             if parsed_json['click'] == 'circle11031':
-                sys.stdout.write('{"arg1": "path11108", "arg2": "fill", "arg3": "set_property", "arg4": "#00ff00"}\r\n')
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "path11108", "style": {"fill": "#ff0000"}}\r\n')
                 sys.stdout.flush()
         except:
             pass
 
         try:
             if parsed_json['mousedown'] == 'circle11035':
-                sys.stdout.write('{"arg1": "path11108", "arg2": "fill", "arg3": "set_property", "arg4": "#0000ff"}\r\n')
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "path11108", "style": {"fill": "#0000ff"}}\r\n')
+                sys.stdout.flush()
+        except:
+            pass
+
+        try:
+            if parsed_json['click'] == 'btn-green-pcb':
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "path7334", "style": {"fill": "#118811"}}\r\n')
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "rect7328", "style": {"fill": "#006600"}, "attrSet": {"stroke":""}}\r\n')
+                sys.stdout.flush()
+        except:
+            pass
+
+        try:
+            if parsed_json['click'] == 'btn-blue-pcb':
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "path7334", "style": {"fill": "#496FA5"}}\r\n')
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "rect7328", "style": {"fill": "#285199"}, "attrSet": {"stroke":""}}\r\n')
+                sys.stdout.flush()
+        except:
+            pass
+
+        try:
+            if parsed_json['click'] == 'btn-red-pcb':
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "path7334", "style": {"fill": "#AA2222"}}\r\n')
+                sys.stdout.write('{"svgId": "svg_img", "elmId": "rect7328", "style": {"fill": "#770000"}, "attrSet":{"stroke":"#F00"}}\r\n')
                 sys.stdout.flush()
         except:
             pass
@@ -79,7 +110,8 @@ sys.stdout.flush()
 
 # dummy action - blinky
 for i in range(3):
-    set_fill = '{"arg1": "%s", "arg2": "fill", "arg3": "set_property", "arg4": "%s"}'+"\r\n"
+    #set_fill = '{"arg1": "%s", "arg2": "fill", "arg3": "set_property", "arg4": "%s"}'+"\r\n"
+    set_fill = '{"svgId": "svg_img", "elmId": "%s", "style": {"fill": "%s"}}'+"\r\n"
 
     led_d16 = "path11108"
     led_pwr = "path11208"
@@ -96,6 +128,18 @@ for i in range(3):
     sys.stdout.write( str((set_fill) % (led_d16, color_white )) )
     sys.stdout.flush()
     time.sleep(0.5)
+
+sys.stdout.write('{"svgId": "svg_img", "elmId": "circle11031", "eventAdd": "click" }\r\n')
+sys.stdout.write('{"svgId": "svg_img", "elmId": "circle11035", "eventAdd": "mousedown" }\r\n')
+sys.stdout.write('{"svgId": "svg_img", "elmId": "polygon10753", "eventAdd": "mouseover" }\r\n')
+sys.stdout.write('{"svgId": "svg_img", "elmId": "polygon10753", "eventAdd": "mouseout" }\r\n')
+
+# Adds events to buttons and sets attribute disabled to empty 
+sys.stdout.write('{"elmId": "btn-green-pcb", "eventAdd": "click", "attrRem": {"disabled":""}}\r\n')
+sys.stdout.write('{"elmId": "btn-blue-pcb", "eventAdd": "click", "attrRem": {"disabled":""}}\r\n')
+sys.stdout.write('{"elmId": "btn-red-pcb", "eventAdd": "click", "attrRem": {"disabled":""}, "attrSet": { "class": ["one","two","three"] }}\r\n')
+sys.stdout.write('{"elmId": "btn-test", "eventAdd": "click", "attrRem": {"disabled":""}, "attrRem": { "class": "" }}\r\n')
+sys.stdout.flush()
 
 # endless loop
 while(1):
